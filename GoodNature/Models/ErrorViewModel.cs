6621c9e11153a833201b5222578517529bf884d3
jonboland <jon.boland@gmail.com>
2022-02-22 14:04:09 +0000

    Add template files

---
+++ GoodNature/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoodNature.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
