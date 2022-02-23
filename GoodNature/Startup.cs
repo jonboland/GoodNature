1f4099e8c9d9b6b539f7ed6138590f96418d58b7
jonboland <jon.boland@gmail.com>
2022-02-23 17:57:05 +0000

    Add ApplicationUser class and migrate changes to db

--- GoodNature/Startup.cs
+++ GoodNature/Startup.cs
@@ -32,7 +32,7 @@ namespace GoodNature
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
