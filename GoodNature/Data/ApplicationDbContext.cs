15307be767e15aca674649f62a6958bf3fa9a6e7
jonboland <jon.boland@gmail.com>
2022-02-25 11:50:24 +0000

    limit ApplicationUser field lengths

--- GoodNature/Data/ApplicationDbContext.cs
+++ GoodNature/Data/ApplicationDbContext.cs
@@ -3,16 +3,22 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace GoodNature.Data
 {
     public class ApplicationUser : IdentityUser
     {
+        [StringLength(100)]
         public string FirstName { get; set; }
+        [StringLength(100)]
         public string LastName { get; set; }
+        [StringLength(100)]
         public string Address1 { get; set; }
+        [StringLength(100)]
         public string Address2 { get; set; }
+        [StringLength(10)]
         public string Postcode { get; set; }
     }
 
