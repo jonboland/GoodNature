5fa891c0197479508a5ac8069795da1b1e25049f
jonboland <jon.boland@gmail.com>
2022-02-25 13:03:26 +0000

    Create CategoryItem entity

---
+++ GoodNature/Entities/CategoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GoodNature.Entities
+{
+    public class CategoryItem
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
+        public string Title { get; set; }
+        public DateTime DateTimeItemReleased { get; set; }
+        public int CategoryId { get; set; }
+        public int MediaTypeId { get; set; }
+
+    }   
+}
