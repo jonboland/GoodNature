4ad08fb05a565f663c72306265a43da00178a286
jonboland <jon.boland@gmail.com>
2022-03-01 11:53:13 +0000

    Add Description and MediaTypes to CategoryItem

--- GoodNature/Entities/CategoryItem.cs
+++ GoodNature/Entities/CategoryItem.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GoodNature.Entities
 {
@@ -9,9 +12,11 @@ namespace GoodNature.Entities
         [Required]
         [StringLength(200, MinimumLength = 2)]
         public string Title { get; set; }
-        public DateTime DateTimeItemReleased { get; set; }
+        public string Description { get; set; }
         public int CategoryId { get; set; }
         public int MediaTypeId { get; set; }
-
+        [NotMapped]
+        public virtual ICollection<SelectListItem> MediaTypes { get; set; }
+        public DateTime DateTimeItemReleased { get; set; }
     }   
 }
