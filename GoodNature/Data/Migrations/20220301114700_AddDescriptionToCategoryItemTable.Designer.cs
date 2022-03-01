cec8603a94d839b0cbafe42877f5493ad0fcc8fc
jonboland <jon.boland@gmail.com>
2022-03-01 11:54:25 +0000

    Migrate Description to CategoryItem table

--- GoodNature/Data/Migrations/20220301114700_AddDescriptionToCategoryItemTable.Designer.cs
+++ GoodNature/Data/Migrations/20220301114700_AddDescriptionToCategoryItemTable.Designer.cs
@@ -4,14 +4,16 @@ using GoodNature.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GoodNature.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220301114700_AddDescriptionToCategoryItemTable")]
+    partial class AddDescriptionToCategoryItemTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
