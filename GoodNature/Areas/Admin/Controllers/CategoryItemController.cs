f6203d960d64c6a5e7ea4a3b4dcce59d3f3a6cc5
jonboland <jon.boland@gmail.com>
2022-03-01 14:27:57 +0000

    Add update items options to Category Index

--- GoodNature/Areas/Admin/Controllers/CategoryItemController.cs
+++ GoodNature/Areas/Admin/Controllers/CategoryItemController.cs
@@ -21,9 +21,22 @@ namespace GoodNature.Areas.Admin.Controllers
         }
 
         // GET: Admin/CategoryItem
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int categoryId)
         {
-            return View(await _context.CategoryItem.ToListAsync());
+            List<CategoryItem> categoryItemList = await (
+                 from catItem in _context.CategoryItem
+                 where catItem.CategoryId == categoryId
+                 select new CategoryItem
+                 {
+                     Id = catItem.Id,
+                     Title = catItem.Title,
+                     Description = catItem.Description,
+                     DateTimeItemReleased = catItem.DateTimeItemReleased,
+                     MediaTypeId = catItem.MediaTypeId,
+                 }
+                 ).ToListAsync();
+
+            return View(categoryItemList);
         }
 
         // GET: Admin/CategoryItem/Details/5
