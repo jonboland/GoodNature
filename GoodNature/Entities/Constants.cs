80c526638a41102e94815f5551f385ea6fa55ebd
jonboland <jon.boland@gmail.com>
2022-03-04 17:26:34 +0000

    Add Constants class

---
+++ GoodNature/Entities/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace GoodNature.Entities
+{
+    public class Constants
+    {
+        public const string TitleLengthErrorMessage = "The title's length must be between two and 200 characters.";
+        public const string MissingThumbnailPathErrorMessage = "A thumbnail image path is required.";
+    }
+}
