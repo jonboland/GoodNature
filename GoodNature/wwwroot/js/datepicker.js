f290d7cd14881d4f6636d7c2d8abea5125e99c61
jonboland <jon.boland@gmail.com>
2022-03-04 12:26:17 +0000

    Set maxDate via addMonths function

--- GoodNature/wwwroot/js/datepicker.js
+++ GoodNature/wwwroot/js/datepicker.js
@@ -2,7 +2,17 @@
 
     $(".datepicker").datepicker(
         {
-            dateFormat: "dd/mm/yy"
+            dateFormat: "dd/mm/yy",
+            maxDate: addMonths(new Date(), 1),
         }
     );
+
+    function addMonths(date, numberOfMonths) {
+
+        var month = date.getMonth();
+
+        var milliSeconds = new Date(date).setMonth(month + numberOfMonths);
+
+        return new Date(milliSeconds);
+    }
 });
