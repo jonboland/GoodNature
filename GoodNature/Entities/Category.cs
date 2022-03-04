1cfe4a57da9c256cf9cd90bb0ebfbb3cd71dca3b
jonboland <jon.boland@gmail.com>
2022-03-04 17:28:39 +0000

    Customise Category field error messages and display name

--- GoodNature/Entities/Category.cs
+++ GoodNature/Entities/Category.cs
@@ -7,12 +7,12 @@ namespace GoodNature.Entities
     public class Category
     {
         public int Id { get; set; }
-        
-        [Required]
-        [StringLength(200, MinimumLength = 2)]
+        [Required(ErrorMessage = Constants.TitleLengthErrorMessage)]
+        [StringLength(5, MinimumLength = 2, ErrorMessage = Constants.TitleLengthErrorMessage)]
         public string Title { get; set; }
         public string Description { get; set; }
-        [Required]
+        [Required(ErrorMessage = Constants.MissingThumbnailPathErrorMessage)]
+        [Display(Name = "Thumbnail Image Path")]
         public string ThumbnailImagePath { get; set; }
         [ForeignKey("CategoryId")]
         public virtual ICollection<CategoryItem> CategoryItems { get; set; }
