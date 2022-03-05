982df32648ca73b113bd4b839ef726286095e47d
jonboland <jon.boland@gmail.com>
2022-03-05 11:26:05 +0000

    Place Constants class at top level

---
+++ GoodNature/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace GoodNature.Entities
+{
+    public class Constants
+    {
+        public const string TitleLengthErrorMessage = "The title's length must be between two and 200 characters.";
+        public const string MissingMediaTypeErrorMessage = "Please select an option from the {0} dropdown list.";
+
+        public const string MediaTypeIdDisplayName = "Media Type";
+        public const string DateTimeItemReleasedDisplayName = "Release Date";
+        public const string HTMLContentDisplayName = "HTML Content";
+        public const string VideoLinkDisplayName = "Video Link";
+        public const string ThumbnailImagePathDisplayName = "Thumbnail Image Path";
+    }
+}
--- GoodNature/Entities/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GoodNature.Entities
-{
-    public class Constants
-    {
-        public const string TitleLengthErrorMessage = "The title's length must be between two and 200 characters.";
-        public const string MissingThumbnailPathErrorMessage = "A thumbnail image path is required.";
-    }
-}
