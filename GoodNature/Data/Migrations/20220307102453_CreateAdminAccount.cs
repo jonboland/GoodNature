d1b1a99c37ff164a600dd0e7a5057db2cd872ffb
jonboland <jon.boland@gmail.com>
2022-03-07 10:31:25 +0000

    Add empty CreateAdminAccount migration

---
+++ GoodNature/Data/Migrations/20220307102453_CreateAdminAccount.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoodNature.Data.Migrations
+{
+    public partial class CreateAdminAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
