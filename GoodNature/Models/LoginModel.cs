d6a986e523b79035dc8d662697e52cd951afa7c4
jonboland <jon.boland@gmail.com>
2022-03-09 13:07:16 +0000

    Create LoginModel

---
+++ GoodNature/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoodNature.Models
+{
+    public class LoginModel
+    {
+        
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = Constants.RememberMeDisplayName)]
+        public bool RememberMe { get; set; }
+        public string LoginInvalid { get; set; }
+    }
+}
