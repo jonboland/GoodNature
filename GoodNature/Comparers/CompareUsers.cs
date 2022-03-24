8b5ef79da4fae02e17778e444f5da987a6253e90
jonboland <jon.boland@gmail.com>
2022-03-24 16:46:12 +0000

    Create _UsersListViewPartial and its parent view

---
+++ GoodNature/Comparers/CompareUsers.cs
@@ -0,0 +1,30 @@
+﻿using GoodNature.Areas.Admin.Models;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GoodNature.Comparers
+{
+    public class CompareUsers : IEqualityComparer<UserModel>
+    {
+        public bool Equals(UserModel x, UserModel y)
+        {
+            if (y == null)
+            {
+                return false;
+            }
+                
+            if (x.Id == y.Id)
+            {
+                return true;
+            }
+
+            return false;
+                
+        }
+
+        public int GetHashCode([DisallowNull] UserModel obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
