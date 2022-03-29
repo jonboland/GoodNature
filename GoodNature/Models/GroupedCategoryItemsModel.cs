1f1209c8f6a0fc751a147edaec9fd15513bb6d3f
jonboland <jon.boland@gmail.com>
2022-03-29 17:00:41 +0100

    Create view models

---
+++ GoodNature/Models/GroupedCategoryItemsModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace GoodNature.Models
+{
+    public class GroupedCategoryItemsModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public IGrouping<int, CategoryItemDetailsModel> Items { get; set; }
+    }
+}
