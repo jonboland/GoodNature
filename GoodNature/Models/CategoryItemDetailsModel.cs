e82ed46cf881b1827e30fe066513a220d6ceb9fc
jonboland <jon.boland@gmail.com>
2022-03-29 17:54:42 +0100

    Expand HomeController

--- GoodNature/Models/CategoryItemDetailsModel.cs
+++ GoodNature/Models/CategoryItemDetailsModel.cs
@@ -2,7 +2,7 @@
 {
     public class CategoryItemDetailsModel
     {
-        public int Id { get; set; }
+        public int CategoryId { get; set; }
         public string CategoryTitle { get; set; }
         public int CategoryItemId { get; set; }
         public string CategoryItemTitle { get; set; }
