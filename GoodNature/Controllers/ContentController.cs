a5acffca7b75bb5f18646ecd6152b8c076b867b4
jonboland <jon.boland@gmail.com>
2022-03-31 10:03:19 +0100

    Create ContentController

---
+++ GoodNature/Controllers/ContentController.cs
@@ -0,0 +1,33 @@
+﻿using GoodNature.Data;
+using GoodNature.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodNature.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index(int categoryItemId)
+        {
+            Content content = await (from item in _context.Content
+                                     where item.CategoryItem.Id == categoryItemId
+                                     select new Content
+                                     {
+                                         Title = item.Title,
+                                         VideoLink = item.VideoLink,
+                                         HTMLContent = item.HTMLContent,
+                                     }).FirstOrDefaultAsync();
+
+            return View(content);
+        }
+    }
+}
