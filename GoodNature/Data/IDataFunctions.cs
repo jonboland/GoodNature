79881567fea721d7c76f1196e56abecbef6a5ec8
jonboland <jon.boland@gmail.com>
2022-04-06 14:48:43 +0100

    Create data functions interface and class

---
+++ GoodNature/Data/IDataFunctions.cs
@@ -0,0 +1,12 @@
+﻿using GoodNature.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GoodNature.Data
+{
+    public interface IDataFunctions
+    {
+        Task UpdateUserCategoryEntityAsync(List<UserCategory> userCategoryItemsToDelete, List<UserCategory> userCategoryItemsToAdd);
+
+    }
+}
