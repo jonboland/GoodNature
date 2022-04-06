80d90f79141e33d4124a6dba732227cc2b2262ee
jonboland <jon.boland@gmail.com>
2022-04-06 13:05:02 +0100

    Create CategoriesToUserModel

---
+++ GoodNature/Models/CategoriesToUserModel.cs
@@ -0,0 +1,12 @@
+﻿using GoodNature.Entities;
+using System.Collections.Generic;
+
+namespace GoodNature.Models
+{
+    public class CategoriesToUserModel
+    {
+        public string UserId { get; set; }
+        public ICollection<Category> Categories { get; set; }
+        public ICollection<Category> CategoriesSelected { get; set; }
+    }
+}
