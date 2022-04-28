075b1fd7116f878df657e28ffc0712e83edfa01f
jonboland <jon.boland@gmail.com>
2022-04-28 09:02:24 +0100

    Split GroupedCategoryItemsModels

--- GoodNature/Models/CategoryDetailsModel.cs
+++ GoodNature/Models/CategoryDetailsModel.cs
@@ -5,7 +5,8 @@ namespace GoodNature.Models
 {
     public class CategoryDetailsModel
     {
-        public IEnumerable<GroupedCategoryItemsModel> GroupedCategoryItemsModels { get; set; }
+        public IEnumerable<GroupedCategoryItemsModel> SelectedGroupedCategoryItemsModels { get; set; }
+        public IEnumerable<GroupedCategoryItemsModel> ActiveGroupedCategoryItemsModels { get; set; }
         public IEnumerable<Category> Categories { get; set; }
     }
 }
