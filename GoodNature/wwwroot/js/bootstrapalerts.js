08782c21ce125338c799ff30fc5afff13f941ad7
jonboland <jon.boland@gmail.com>
2022-05-06 13:18:29 +0100

    Use const instead of var

--- GoodNature/wwwroot/js/bootstrapalerts.js
+++ GoodNature/wwwroot/js/bootstrapalerts.js
@@ -4,12 +4,12 @@
         alertType = "info";
     }
 
-    var alertHTML = `<div class="alert alert-${alertType} alert-dismissible fade show" role="alert">`
-        + `<strong>${alertHeading}</strong><br>${alertMessage}`
-        + '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
-        + '<span aria-hidden="true">&times;</span>'
-        + '</button>'
-        + '</div>';
+    const alertHTML = `<div class="alert alert-${alertType} alert-dismissible fade show" role="alert">`
+          + `<strong>${alertHeading}</strong><br>${alertMessage}`
+          + '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+          + '<span aria-hidden="true">&times;</span>'
+          + '</button>'
+          + '</div>';
 
     $(placeHolderElementId).html(alertHTML);
 }
