70f8de26ed693d3903b52594f3ac792e0e6c6fc0
jonboland <jon.boland@gmail.com>
2022-05-09 18:04:51 +0100

    Use EF method

--- GoodNature/Controllers/ContentController.cs
+++ GoodNature/Controllers/ContentController.cs
@@ -9,16 +9,16 @@ namespace GoodNature.Controllers
 {
     public class ContentController : Controller
     {
-        private ICustomDataMethods _customDataMethods;
+        private ApplicationDbContext _context;
 
-        public ContentController(ICustomDataMethods customDataMethods)
+        public ContentController(ApplicationDbContext context)
         {
-            _customDataMethods = customDataMethods;
+            _context = context;
         }
         
         public async Task<IActionResult> Index(int categoryItemId)
         {
-            Content content = await _customDataMethods.GetPieceOfContent(categoryItemId);
+            Content content = await _context.Content.FirstOrDefaultAsync(item => item.CategoryItem.Id == categoryItemId);
 
             return View(content);
         }
